172c29c2d5e8bf6402a067e5302a844b7be21cc8
fenny7646 <fennyvarghese1@gmail.com>
2025-01-18 00:21:17 +0530

    C0 | January 18,12:20 AM | Main>Updated Shell

--- pusher.sh
+++ pusher.sh
@@ -3,8 +3,8 @@
 # File to store the counter
 COUNTER_FILE=".commit_counter"
 
-# Define an array of remote repositories and their values
-declare -A remote_values=( ["origin"]="" ["traveller"]=1 )
+# Define an array of branches and their values
+declare -A branch_values=( ["travell"]=1 )
 
 # Define an array of remote repositories and their values
 declare -A file_values=(  ["Main"]=0 ["Templates"]=1 ["Static"]=2 ["Media"]=3)
@@ -13,6 +13,27 @@ declare -A file_values=(  ["Main"]=0 ["Templates"]=1 ["Static"]=2 ["Media"]=3)
 if [ ! -f "$COUNTER_FILE" ]; then
     echo 0 > "$COUNTER_FILE"
 fi
+
+changer() {
+    # Run Git commands (example)
+    echo "Staging changes..."
+    if ! git add .; then
+        echo "Error: Failed to stage changes."
+        
+    fi
+    echo "Committing changes..."
+    if ! git commit -m "$1"; then
+        echo "Error: Failed to commit changes. Make sure there are changes to commit."
+
+    fi
+    echo "Pushing changes to $2..."
+    if ! git push -u "$2" "$3"; then
+        echo "Error: Failed to push changes to $2."
+        exit 1
+    fi
+    echo "Changes pushed successfully with commit message: $1"
+}
+
 
 # Read the current counter value
 counter=$(cat "$COUNTER_FILE")
@@ -41,11 +62,11 @@ fi
 current_date=$(date +"%B %d,%-I:%M %p")
 
 # Display available Initial Text and prompt user to select one
-echo "Available remote repositories:"
+echo "Available  FIle Text for Commit:"
 
 for key in "${!file_values[@]}"; do
     value=${file_values[$key]}
-    echo "$value: $key"
+    echo "$value : $key"
 done
 
 read -p "Select initial file text by index (press enter to skip): " file_index
@@ -54,7 +75,7 @@ if [[ -z "$file_index" ]]; then
     selected_file=""  # Set file to an empty value
 else
     for key in "${!file_values[@]}"; do
-        if [[ "${file_values[$key]}" == "$file" ]]; then
+        if [[ "${file_values[$key]}" == "$file_index" ]]; then
             selected_file="$key>"
             break
         fi
@@ -70,40 +91,39 @@ formatted_message="C${counter} | ${current_date} | ${selected_file}${commit_mess
 echo "Commit message: $formatted_message"
 
 # Display available remote repositories and prompt user to select one
-echo "Available remote repositories:"
-for i in "${!remote_repos[@]}"; do
-    echo "$i: ${remote_repos[$i]}"
+echo "Available  branches:"
+for key in "${!branch_values[@]}"; do
+    value=${branch_values[$key]}
+    echo "$key : $value"
 done
 
-read -p "Select a remote repository by index (default: 0 for 'origin'): " repo_index
+read -p "Select a branch by index (default:'main'): " branch_index
 
-# Default to 'origin' if no input is provided
-repo_index=${repo_index:-0}
-selected_repo=${remote_repos[$repo_index]}
-
-
-# Run Git commands (example)
-echo "Staging changes..."
-if ! git add .; then
-    echo "Error: Failed to stage changes."
-    exit 1
+if [[ -z "$branch_index" ]]; then
+    selected_branch="main"  # Set file to an empty value
+else
+    for key in "${!branch_values[@]}"; do
+        if [[ "${branch_values[$key]}" == "$branch_index" ]]; then
+            selected_branch="$key"
+            break
+        fi
+    done
 fi
 
-echo "Committing changes..."
-if ! git commit -m "$formatted_message"; then
-    echo "Error: Failed to commit changes. Make sure there are changes to commit."
-    exit 1
+read -p "Enter repo remote name:" selected_repo
+echo "Carefully verify bellow actions:"
+echo "-----------------------------------------------------------"
+echo "git commit -m $formatted_message"
+echo "git push -u $selected_repo $selected_branch"
+echo "-----------------------------------------------------------"
+read -p "Do you want to continue[Y/N]:" confirmer
+if [[ "$confirmer" == "Y" || "$confirmer" == "y" ]]; then
+    changer "$formatted_message" "$selected_repo" "$selected_branch"
+    # Increment the counter
+    new_counter=$((counter + 1))
+    # Save the updated counter back to the file for future use
+    echo "$new_counter" > "$COUNTER_FILE"
+else
+    break
 fi
 
-echo "Pushing changes to $selected_repo..."
-if ! git push -u "$selected_repo" travell; then
-    echo "Error: Failed to push changes to $selected_repo."
-    exit 1
-fi
-
-echo "Changes pushed successfully with commit message: $formatted_message"
-
-# Increment the counter
-new_counter=$((counter + 1))
-# Save the updated counter back to the file for future use
-echo "$new_counter" > "$COUNTER_FILE"
